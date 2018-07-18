45845495cf5a19af4c0b111243bc71eb4b5f7dfc
Kevin Sohngen <sohngen.5@osu.edu>
2018-07-18 10:29:53 -0400

    A

--- src/main/java/com/techelevator/npgeek/JdbcParkDao.java
+++ src/main/java/com/techelevator/npgeek/JdbcParkDao.java
@@ -1,5 +1,53 @@
 package com.techelevator.npgeek;
 
-public class JdbcParkDao {
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+public class JdbcParkDao implements ParkDao{
+	
+	private JdbcTemplate jdbcTemplate;
+
+
+	@Override
+	public List<Park> getAllParks() {
+		List<Park> allParks = new ArrayList();
+		String selectAllParks = "SELECT * FROM park";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(selectAllParks);
+		while (results.next()) {
+			Park park = new Park();
+			park.setParkCode(results.getString("parkcode"));
+			park.setParkName(results.getString("parkname"));
+			park.setState(results.getString("state"));
+			park.setAcreage(results.getInt("acreage"));
+			park.setElevationInFeet(results.getInt("elevationinfeet"));
+			park.setMilesOfTrail(results.getFloat("milesoftrail"));
+			park.setNumberOfCampsites(results.getInt("numberofcampsites"));
+			park.setClimate(results.getString("climate"));
+			park.setYearFounded(results.getInt("yearfounded"));
+			park.setAnnualVisitorCount(results.getInt("annualvisitorcount"));
+			park.setInspirationalQuote(results.getString("inspirationalquote"));
+			park.setInspirationalQuoteSource(results.getString("inspirationalquotesource"));
+			park.setParkDescription(results.getString("parkdescription"));
+			park.setEntryFee(results.getInt("entryfee"));
+			park.setNumberOfAnimalSpecies(results.getString("numberofanimalspecies"));
+			allParks.add(park);
+		}
+		return allParks;
+	}
+
+	@Override
+	public Park getSellectedPark() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void saveInput() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
