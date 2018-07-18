4d4127695e0719a204483c95dbe58c65499c0c8a
Kevin Sohngen <sohngen.5@osu.edu>
2018-07-18 10:21:29 -0400

    Asdf

--- src/main/java/com/techelevator/npgeek/ParkController.java
+++ src/main/java/com/techelevator/npgeek/ParkController.java
@@ -1,11 +1,15 @@
 package com.techelevator.npgeek;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ParkController {
+	
+	@Autowired
+	private ParkDao parkDao;
 	
 	@RequestMapping(path= {"/"}, method=RequestMethod.GET)
 	public String HomePage() {
