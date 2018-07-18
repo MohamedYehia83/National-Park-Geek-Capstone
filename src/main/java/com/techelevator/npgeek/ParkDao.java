9687b22d9d1567942715cf48292e328a3a4699ec
Mohamed Yehia <massem1983@yahoo.com>
2018-07-18 10:16:02 -0400

    updated

--- src/main/java/com/techelevator/npgeek/ParkDao.java
+++ src/main/java/com/techelevator/npgeek/ParkDao.java
@@ -1,5 +1,14 @@
 package com.techelevator.npgeek;
 
+import java.util.List;
+
 public interface ParkDao {
 
+	public List<Park> getAllParks();
+	
+	public Park getSellectedPark() ;
+	
+	public void saveInput();
+		
+	
 }
